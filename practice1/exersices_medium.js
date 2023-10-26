6ab1f8ee368977cd14cec1340f20637c3505657f
Yauheni Pratasevich <evgen.polotsk@gmail.com>
2023-10-26 11:07:44 +0300

    feat: finish medium task

---
+++ practice1/exersices_medium.js
@@ -0,0 +1,13 @@
+// (*) Продвинутые задания на использование циклов и условий
+
+function firstTask() {
+  const arr = [3, 5, 8, 16, 20, 23, 50];
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+  }
+  return result;
+}
+
+firstTask();
