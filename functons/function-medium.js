198654afc7b0e54140f67147d1ebcdc81ae9a192
Yauheni Pratasevich <evgen.polotsk@gmail.com>
2023-10-29 00:05:26 +0300

    feat: finish function-medium

---
+++ functons/function-medium.js
@@ -0,0 +1,18 @@
+// Место для первой задачи
+function calculateVolumeAndArea(length) {
+  if (typeof length !== "number" || length < 0 || !Number.isInteger(length)) {
+    return "При вычислении произошла ошибка";
+  }
+
+  let volume = 0,
+    area = 0;
+
+  volume = length * length * length;
+  // length ** 3 - это тоже самое, что и выше или варианты через цикл.
+  // ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает =/
+  area = 6 * (length * length);
+
+  return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+}
+
+console.log(calculateVolumeAndArea(5));
