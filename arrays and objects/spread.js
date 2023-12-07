9b707e53059636472b46c2bede33112c27a0d915
Yauheni Pratasevich <evgen.polotsk@gmail.com>
2023-12-07 16:06:54 +0300

    feat: add objects tasks

--- arrays and objects/spread.js
+++ arrays and objects/spread.js
@@ -86,4 +86,38 @@ const q = {
     two: 2
 }
 
-const newObjj = {...q}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test')
+    }
+}
+
+options.makeTest();
+
+// let counter = 0;
+// for(let key in options) {
+//     if (typeof options[key] === 'object') {
+//         for(let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+//             counter++
+//         }
+//     } else {
+//        console.log(`Свойство ${key} имеет значение ${options[key]}`)
+//        counter++
+//     }
+// }
+// console.log(counter)
+
+
+// let arr = Object.keys(options).length;
+// console.log(arr)
+
+const {border, bg} = options.colors;
+console.log(border)
--- arrays and objects/task1.js
+++ arrays and objects/task1.js
@@ -1,3 +1,9 @@
+// 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
+
+// Пример:
+
+// showExperience(personalPlanPeter) => '1 month'
+
 const personalPlanPeter = {
     name: "Peter",
     age: "29",
@@ -5,29 +11,31 @@ const personalPlanPeter = {
         languages: ['ru', 'eng'],
         programmingLangs: {
             js: '20%',
-            php: '10%',
-            c: '200%'
+            php: '10%'
         },
         exp: '1 month'
     }
 };
 
 function showExperience(plan) {
-    const {exp} = plan.skills;
-    return exp
+    return plan.skills.exp
 }
 
-console.log(showExperience(personalPlanPeter));
+console.log(showExperience(personalPlanPeter))
+
+// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+
+// Пример:
+
+// showProgrammingLangs(personalPlanPeter)  =>
+
+// "Язык js изучен на 20% Язык php изучен на 10%"
+
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+
+// P.S. Для переноса строки используется \n в конце строки.
+
 
 function showProgrammingLangs(plan) {
-    let str = '';
-    const {programmingLangs} = plan.skills;
-    for (let key in programmingLangs) {
-        str += `Язык ${key} изучен на ${programmingLangs[key]}\n` 
-    }    
-    return str;
+    let str = `Язык ${plan.skills.programmingLangs}`
 }
-
-console.log(showProgrammingLangs(personalPlanPeter))
-
-
